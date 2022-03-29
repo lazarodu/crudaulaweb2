63e494b33650bc260180d0be8a24beafde05ecbb
lazarodu <lazarodu@gmail.com>
2022-03-29 08:32:49 -0300

    Aula do dia 29/03/2022

---
+++ controller/Teste.php
@@ -0,0 +1,8 @@
+<?php
+class Teste
+{
+  public function __construct()
+  {
+    echo '<h1>Teste</h1>';
+  }
+}
---
+++ index.php
@@ -0,0 +1,26 @@
+
+<?php
+// auto load
+spl_autoload_extensions('.php');
+function classLoader($class)
+{
+  $nomeArquivo = $class . '.php';
+  $pastas = array('controller', 'model');
+  foreach ($pastas as $pasta) {
+    $arquivo = "{$pasta}/{$nomeArquivo}";
+    if (file_exists($arquivo)) {
+      require_once($arquivo);
+    }
+  }
+}
+spl_autoload_register('classLoader');
+// Front Controller
+class Aplicacao
+{
+  public static function run()
+  {
+    if (isset($_GET["acao"])) {
+      echo $_GET["acao"];
+    }
+  }
+}
