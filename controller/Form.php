881cef5e9360048e961142a597123bfb97bc2435
lazarodu <lazarodu@gmail.com>
2022-04-19 10:32:23 -0300

    Aula do dia 19/04/2022

--- controller/Form.php
+++ controller/Form.php
@@ -9,7 +9,18 @@ class Form
   }
   public function salvar()
   {
-    print_r($_POST);
+    if (isset($_POST['marca']) && isset($_POST['configuracao']) && isset($_POST['valor'])) {
+      try {
+        $conexao = Transaction::get();
+        $computador = new Crud('computador');
+        $marca = $conexao->quote($_POST['marca']);
+        $configuracao = $conexao->quote($_POST['configuracao']);
+        $valor = $conexao->quote($_POST['valor']);
+        $resultado = $computador->insert("marca,configuracao,valor", "$marca,$configuracao,$valor");
+      } catch (Exception $e) {
+        echo $e->getMessage();
+      }
+    }
   }
   public function getMessage()
   {
