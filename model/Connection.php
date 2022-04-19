a37962d744ac703fedfe917c9aa87bf997260f5a
lazarodu <lazarodu@gmail.com>
2022-04-19 09:41:02 -0300

    Aula do dia 19/04/2022

---
+++ model/Connection.php
@@ -0,0 +1,37 @@
+<?php
+final class Connection
+{
+  private function __construct()
+  {
+  }
+  public static function open()
+  {
+    $type = "mysql";
+    $user = "root";
+    $pass = "mariadb";
+    $name = "cefet1";
+    $host = "mariadb";
+    switch ($type) {
+      case 'pgsql':
+        $conn = new PDO("pgsql:dbname={$name};host={$host}", $user, $pass);
+        break;
+
+      case 'mysql':
+        $conn = new
+          PDO("mysql:host={$host};port=3306;dbname={$name}", $user, $pass);
+        break;
+      case 'sqlite':
+        $conn = new PDO("sqlite:{$name}");
+        break;
+      case 'ibase':
+        $conn = new PDO("firebird:dbname={$name}", $user, $pass);
+        break;
+      case 'oci8':
+        $conn = new PDO("oci:dbname={$name}", $user, $pass);
+        break;
+      case 'mssql':
+        $conn = new PDO("mssql:host={$host},1433;dbname={$name}", $user, $pass);
+        break;
+    }
+  }
+}
