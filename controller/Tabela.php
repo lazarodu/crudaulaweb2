3e14d43e98697d7209fae5879a239f9ca0c60a3c
lazarodu <lazarodu@gmail.com>
2022-04-26 10:07:59 -0300

    Aula do dia 26/04/2022

--- controller/Tabela.php
+++ controller/Tabela.php
@@ -2,12 +2,23 @@
 class Tabela
 {
   private $message = "";
+  public function __construct()
+  {
+    Transaction::open();
+  }
   public function controller()
   {
-    $this->message = "Estou na classe Tabela";
+    Transaction::get();
+    $computador = new Crud("computador");
+    $resultado = $computador->select();
+    print_r($resultado);
   }
   public function getMessage()
   {
     return $this->message;
+  }
+  public function __destruct()
+  {
+    Transaction::close();
   }
 }
